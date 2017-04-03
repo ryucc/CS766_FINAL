ee3a29811945d0a6a67cd2632e10f79ac4ff9a12
m4a11205 <m4a11205@gamil.com>
2017-04-03 14:55:23 -0500

    update

--- together.py
+++ together.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import cv2
+import numpy as np
 import sys, os
+
 def combine(orig_img,warped_img):
     height1 = orig_img.shape[0]
     width1 = orig_img.shape[1]
@@ -17,23 +19,35 @@ def combine(orig_img,warped_img):
         temp[:height1,:width1,:] = orig_img
         temp[:,width1:width1+width2,:] = warped_img
     return temp
- 
+
 if __name__ == "__main__":
+    fileName = sys.argv[3]
+    try:
+        os.remove(fileName)
+    except:
+        print "No such file!"
+
     fourcc = cv2.cv.CV_FOURCC(*'X264')
     cap1 = cv2.VideoCapture(sys.argv[1])
     cap2 = cv2.VideoCapture(sys.argv[2])
-    ret1, frame1 = cap.read()
-    ret2, frame2 = cap.read()
-    height , width , layers =  frame.shape
-    out = cv2.VideoWriter(sys.argv[3], fourcc, 30.0, (width*2,height))
-    out.write(combine(frame1,frame2)
+    ret1, frame1 = cap1.read()
+    ret2, frame2 = cap2.read()
+    height , width , layers =  frame1.shape
+
+    out = cv2.VideoWriter(fileName, fourcc, 20.0, (width*2,height))
+    out.write(combine(frame1,frame2))
+
     while(cap1.isOpened() and cap2.isOpened()):
         ret1, frame1 = cap1.read()
         ret2, frame2 = cap2.read()
-        if ret1==True:
+        if (ret1 and ret2):
             #frame = cv2.flip(frame,0)
             # write the flipped frame
             frame = combine(frame1,frame2)
             out.write(frame)
         else:
             break
+
+    out.release()
+    cap1.release()
+    cap2.release()
